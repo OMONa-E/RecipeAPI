586358f3a7c72675bbefe93ae668673a2faca4cf
Omona Emmanuel <omonaemman@gmail.com>
2024-09-30 22:55:54 +0300

    remove extension logger, out of app context

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -1,12 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
-from flask import current_app
 
 
 # # Instantiate the extensions
 db = SQLAlchemy()
 migrate = Migrate()
 jwt = JWTManager()
-
-current_app.logger.info(f'Extensions instantiated successfully')
